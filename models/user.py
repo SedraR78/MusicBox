b1a30167cee8fe8a116a721dbf896d0155600e56
Sedra Ramarosaona <sedranante@gmail.com>
2026-04-14 23:48:20 +0200

    feat: add User model

---
+++ models/user.py
@@ -0,0 +1,10 @@
+from models.database import db
+from .basemodel import BaseModel  
+
+class User(BaseModel):
+    __tablename__ = 'users'
+
+    name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password =  db.Column(db.String(128), nullable=False)
+    role = db.Column(db.Enum('boxer','contributor','admin'), default= 'boxer' )
